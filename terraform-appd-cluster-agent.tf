cec40b454c162e58914ebb560af40db22ff3d4e7
Majid S <m.sotoodeh@gmail.com>
2022-03-30 02:09:59 -0700

    Update terraform-appd-cluster-agent.tf

--- terraform-appd-cluster-agent.tf
+++ terraform-appd-cluster-agent.tf
@@ -49,12 +49,12 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = "da-compute-kubeconfig.yml" # NOTE: Change this value to your kubeconfig file
+  config_path = "IKS-Cluster01-kubeconfig.yml" # NOTE: Change this value to your kubeconfig file
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "da-compute-kubeconfig.yml"
+    config_path = "IKS-Cluster01-kubeconfig.yml"
   }
 }
 
