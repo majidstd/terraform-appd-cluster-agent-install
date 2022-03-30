f8cb522c9e9c443c6da62f83437ecfc917dd78fc
Majid S <m.sotoodeh@gmail.com>
2022-03-30 13:17:11 -0700

    update

--- terraform-appd-cluster-agent.tf
+++ terraform-appd-cluster-agent.tf
@@ -85,6 +85,7 @@ resource "kubernetes_namespace" "metrics" {
 # ]
 #
 #}
+
 # Deploy the AppDynamics cluster agent using AppDynamic's helm chart
 resource "helm_release" "cluster-agent" {
   name = "cluster-agent"
